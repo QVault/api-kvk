859202ab50354184f0f84502c164f294d9f0856c
Quincent <mail@quincent.com>
2023-12-12 04:48:38 -0400

    Refactor saveResponseToD1 to saveResponseToR2

--- src/api/kvk/fetchDetailsForBusinesses.ts
+++ src/api/kvk/fetchDetailsForBusinesses.ts
@@ -2,10 +2,21 @@ import { HandelRegisterResponse } from '../../models/typesHandelRegisterAPI';
 import { Dossier } from '../../models/typesBedrijfRegisterAPI';
 import { Context } from 'hono';
 
-async function saveResponseToD1(env: Context, dossierCode: string, url: string, data: string): Promise<void> {
-	const insertQuery = `INSERT INTO your_table_name (DOSSIER_CODE, URL, DATA) VALUES (?, ?, ?)`;
-	const statement = env.DB.prepare(insertQuery);
-	await statement.bind(dossierCode, url, data).all();
+// async function saveResponseToD1(c: Context, dossierCode: string, url: string, data: string): Promise<void> {
+// 	const insertQuery = `INSERT INTO json_data (DOSSIER_CODE, URL, DATA) VALUES (?, ?, ?)`;
+// 	const statement = c.env.DB.prepare(insertQuery);
+// 	await statement.bind(dossierCode, url, data).all();
+// }
+
+async function saveResponseToR2(c: Context, dossierCode: string, data: any): Promise<void> {
+	try {
+		const dataToSave = typeof data === 'string' ? data : JSON.stringify(data);
+
+		await c.env.R2.put(dossierCode, dataToSave);
+	} catch (error) {
+		console.error('Error saving to R2:', error);
+		throw error;
+	}
 }
 
 async function fetchDetailsForBusinesses(env: Context, searchResults: Dossier[]): Promise<any[]> {
@@ -13,6 +24,7 @@ async function fetchDetailsForBusinesses(env: Context, searchResults: Dossier[])
 		searchResults.map(async (business) => {
 			const coreCode = business.dossiernummer.registratienummer;
 			const branchId = business.dossiernummer.filiaalnummer;
+			const dossierCode = coreCode.toString() + '.' + branchId.toString();
 			const businessType = business.dossiernummer.bedrijfstype.name;
 			const detailUrl = `https://api.arubachamber.com/api/v1/bedrijf/public/details/${businessType}/${coreCode}/${branchId}`;
 			// console.log('Fetching details for:', detailUrl);
@@ -20,7 +32,7 @@ async function fetchDetailsForBusinesses(env: Context, searchResults: Dossier[])
 			const detailResponse = await fetch(detailUrl);
 			const jsonResponse = await detailResponse.json();
 
-			await saveResponseToD1(env, coreCode.toString(), detailUrl, JSON.stringify(jsonResponse));
+			await saveResponseToR2(env, dossierCode, JSON.stringify(jsonResponse));
 			return jsonResponse as HandelRegisterResponse;
 		})
 	);
--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -5,11 +5,19 @@ export type Environment = {
 	SUPABASE_KEY: string;
 	KVK_REGISTRY: KVNamespace;
 	DB: D1Database;
+	R2: R2Bucket;
 };
+
+interface R2Bucket {
+	get(key: string, type?: 'text' | 'json' | 'arrayBuffer' | 'stream'): Promise<string | ArrayBuffer | ReadableStream | null>;
+	put(key: string, value: string | ReadableStream | ArrayBuffer | FormData, options?: KVNamespacePutOptions): Promise<void>;
+	delete(key: string): Promise<void>;
+}
 
 export interface Env extends Environment {
 	SUPABASE_URL: string;
 	SUPABASE_KEY: string;
 	KVK_REGISTRY: KVNamespace;
 	DB: D1Database;
+	R2: R2Bucket;
 }
