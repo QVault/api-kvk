fee17a144c7a5b92028f844a618bb7a74de84583
Quincent <mail@quincent.com>
2023-12-14 17:57:25 -0400

    Changed `SupabaseClient` initialization to use singleton pattern

--- src/api/supabase/supabaseClient.ts
+++ src/api/supabase/supabaseClient.ts
@@ -1,12 +1,14 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { Context } from 'hono';
+let supabase: SupabaseClient;
 
-function initSupabaseClient(c: Context): SupabaseClient {
-	const SUPABASE_URL: string = c.env.SUPABASE_URL;
-	const SUPABASE_KEY: string = c.env.SUPABASE_KEY;
-
-	const supabase: SupabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+function getSupabaseClient(c: Context): SupabaseClient {
+	if (!supabase) {
+		const SUPABASE_URL: string = c.env.SUPABASE_URL;
+		const SUPABASE_KEY: string = c.env.SUPABASE_KEY;
+		supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+	}
 	return supabase;
 }
 
-export { initSupabaseClient };
+export { getSupabaseClient };
